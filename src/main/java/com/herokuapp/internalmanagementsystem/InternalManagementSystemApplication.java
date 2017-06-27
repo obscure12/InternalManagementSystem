4e387dfed933640cf2d199e0dacf08eccdcb8719
obscure12 <longwangc12@gmail.com>
2017-06-27 21:05:25 +0900

    プロジェクト作成

---
+++ src/main/java/com/herokuapp/internalmanagementsystem/InternalManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package com.herokuapp.internalmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternalManagementSystemApplication.class, args);
+	}
+}
